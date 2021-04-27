facdfc873bf0ec048bad65cec38fdcbfeae165cb
Chung Tran <tdchung.9@gmail.com>
2021-04-27 11:19:45 +0700

    update send data, handle RPC

--- main/config.h
+++ main/config.h
@@ -8,7 +8,26 @@
 
 
 // MQTT info
-#define CONFIG_MQTT_BROKER_URL "54.213.207.33"
-#define CONFIG_MQTT_TOKEN "Y4qhdqBXgDtUGs0wignB"
+#define CONFIG_MQTT_BROKER_URL "thingsboard.cloud"
+#define PUBLISH_TELEMETRY "v1/devices/me/telemetry"
+#define PUBLISH_ATTIBUTES "v1/devices/me/attributes"
+#define SERVER_RPC "v1/devices/me/rpc/request/+"
+#define PUBLISH_CLIENT_RPC "v1/devices/me/rpc/response/"
 
 
+// TODO: this token is the chip ID.  the code will be updated following it
+#define CONFIG_MQTT_TOKEN "840D8E0BC304"
+
+
+
+
+
+const char *THINGBOARD_SERVER = "scandiasaunas.com";
+
+const char *TOPIC_P_TELEMETRY = "v1/devices/me/telemetry";
+const char *TOPIC_P_ATTRIBUTES = "v1/devices/me/attributes";
+const char *TOPIC_P_CLIENT_RPC = "v1/devices/me/rpc/response/";
+const char *TOPIC_S_SERVER_RPC = "v1/devices/me/rpc/request/+";
+const char *TOPIC_P_CLAIM = "v1/devices/me/claim";
+
+const uint64_t CLAIM_TIMEOUT = 60000 * 5;
--- main/main.c
+++ main/main.c
@@ -25,6 +25,7 @@
 #include "lwip/dns.h"
 #include "lwip/netdb.h"
 
+#include "cJSON.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
@@ -40,15 +41,13 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
-#define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+#define EXAMPLE_ESP_WIFI_SSID CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
 
 // MQTT
-#define EXAMPLE_ESP_MQTT_URL        CONFIG_MQTT_BROKER_URL
-#define EXAMPLE_ESP_MQTT_TOKEN      CONFIG_MQTT_TOKEN
-
-
+#define EXAMPLE_ESP_MQTT_URL CONFIG_MQTT_BROKER_URL
+#define EXAMPLE_ESP_MQTT_TOKEN CONFIG_MQTT_TOKEN
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
@@ -57,28 +56,36 @@ static EventGroupHandle_t s_wifi_event_group;
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
 #define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+#define WIFI_FAIL_BIT BIT1
 
-static const char *TAG = "wifi station";
+static const char *TAG = "WIFI_MQTT";
 
 static int s_retry_num = 0;
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
         esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
             esp_wifi_connect();
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
+        }
+        else
+        {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
@@ -117,37 +124,41 @@ void wifi_init_sta(void)
             /* Setting a password implies station will connect to all security modes including WEP/WPA.
              * However these modes are deprecated and not advisable to be used. Incase your Access point
              * doesn't support WPA2, these mode can be enabled by commenting below line */
-	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
 
             .pmf_cfg = {
                 .capable = true,
-                .required = false
-            },
+                .required = false},
         },
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 
     /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
      * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
 
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
      * happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
+    if (bits & WIFI_CONNECTED_BIT)
+    {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-    } else if (bits & WIFI_FAIL_BIT) {
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-    } else {
+    }
+    else
+    {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 
@@ -162,53 +173,87 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
     esp_mqtt_client_handle_t client = event->client;
     int msg_id;
     // your_context_t *context = event->context;
-    switch (event->event_id) {
-        case MQTT_EVENT_CONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
-            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+    switch (event->event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        ESP_LOGI(TAG, "send attributes to thingsboard server");
+        
+        // test send attributes and telemetry
+        msg_id = esp_mqtt_client_publish(client, PUBLISH_ATTIBUTES, "{\"ip\":\"10.10.10.10\"}", 0, 1, 0);
+        msg_id = esp_mqtt_client_publish(client, PUBLISH_TELEMETRY, "{\"temperature\":42}", 0, 1, 0);
+        
+        // subscribe to the RPC chanel
+        ESP_LOGI(TAG, "Subscribe to RPC");
+        msg_id = esp_mqtt_client_subscribe(client, SERVER_RPC, 1);
 
-            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
 
-            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
 
-            msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-            ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-            break;
-        case MQTT_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-            break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
 
-        case MQTT_EVENT_SUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-            break;
-        case MQTT_EVENT_UNSUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-            break;
-        case MQTT_EVENT_PUBLISHED:
-            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-            break;
-        case MQTT_EVENT_DATA:
-            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-            printf("DATA=%.*s\r\n", event->data_len, event->data);
-            break;
-        case MQTT_EVENT_ERROR:
-            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-            break;
-        default:
-            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-            break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_DATA:
+        // new incomming data here
+        // TODO: handle PRC command
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+
+        // test phase data
+        cJSON *msg = cJSON_Parse(event->data);
+        char *method = cJSON_GetObjectItem(msg, "method")->valuestring;
+        cJSON *params = cJSON_GetObjectItem(msg,"params");
+        ESP_LOGI(TAG, "method=%s", method);
+        ESP_LOGI(TAG, "params=%s", cJSON_Print(params));
+
+        // delete
+        // cJSON_Delete(params); 
+        cJSON_Delete(msg);
+
+        // get RPC id 
+        ESP_LOGI(TAG, "HERE");
+        char requestId[10] = {0};
+        strncpy(requestId, event->topic + (strlen(SERVER_RPC) -1), event->topic_len - (strlen(SERVER_RPC)-1));
+        requestId[event->topic_len - (strlen(SERVER_RPC)-1)] = '\0';
+        ESP_LOGI(TAG, "HERE2");
+        ESP_LOGI(TAG, "ID=%s", requestId);
+
+        // send ACK
+        char topic_response[50] = {0};
+        strcpy(topic_response, PUBLISH_CLIENT_RPC);
+        strcat(topic_response, requestId);
+        // just test
+        msg_id = esp_mqtt_client_publish(client, topic_response, "{\"ack\":\"lala\"}", 0, 1, 0);
+
+
+
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
     }
     return ESP_OK;
 }
 
-
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
     mqtt_event_handler_cb(event_data);
 }
@@ -226,23 +271,58 @@ static void mqtt_app_start(void)
     esp_mqtt_client_start(client);
 }
 
+static esp_err_t get_esp32_mac(uint8_t *mac)
+{
+    return esp_efuse_mac_get_default(mac);
+}
+
+
+// test json
+static void test_json()
+{
+    ESP_LOGI(TAG, "Serialize.....");
+    cJSON *root;
+    root = cJSON_CreateObject();
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    cJSON_AddStringToObject(root, "version", IDF_VER);
+    cJSON_AddNumberToObject(root, "cores", chip_info.cores);
+    cJSON_AddTrueToObject(root, "flag_true");
+    cJSON_AddFalseToObject(root, "flag_false");
+    const char *my_json_string = cJSON_Print(root);
+    ESP_LOGI(TAG, "my_json_string\n%s", my_json_string);
+    cJSON_Delete(root);
+
+    ESP_LOGI(TAG, "Deserialize.....");
+    cJSON *root2 = cJSON_Parse(my_json_string);
+    char *version = cJSON_GetObjectItem(root2, "version")->valuestring;
+    int cores = cJSON_GetObjectItem(root2, "cores")->valueint;
+    bool flag_true = cJSON_GetObjectItem(root2, "flag_true")->valueint;
+    bool flag_false = cJSON_GetObjectItem(root2, "flag_false")->valueint;
+    ESP_LOGI(TAG, "version=%s", version);
+    ESP_LOGI(TAG, "cores=%d", cores);
+    ESP_LOGI(TAG, "flag_true=%d", flag_true);
+    ESP_LOGI(TAG, "flag_false=%d", flag_false);
+    cJSON_Delete(root2);
+}
 
 void app_main(void)
 {
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
     // test get chip id
     uint8_t baseMac[6];
     // esp_efuse_read_mac(baseMac);
-    esp_efuse_mac_get_default(baseMac);
+    get_esp32_mac(baseMac);
     ESP_LOGI(TAG, "ID: %02X%02X%02X%02X%02X%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
-
+    test_json();
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
